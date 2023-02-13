404f6e4156849c5bda946be7cdc4fedb1c6a0026
Zbyna Stuchlik <zbynek.stuchlik@gmail.com>
2023-02-13 15:08:05 +0100

    add age

--- emptyProject/src/greet.java
+++ emptyProject/src/greet.java
@@ -1,7 +1,7 @@
 public class greet {
   
-  public static void greet(String parameter){
-    System.out.println("Greetings from the " + parameter);
+  public static void greet(String parameter, int age){
+    System.out.println("Greetings from the " + parameter + age);
 
   }
 
